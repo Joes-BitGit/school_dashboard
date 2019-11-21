53da3d2f5beee4fb1c7fb007abfad92162c14d11
Joes-BitGit <josephnalmeida@gmail.com>
2019-11-20 17:42:53 -0800

    Skeleton of application

--- venv/webapp/app.py
+++ venv/webapp/app.py
@@ -1,10 +1,32 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/login')
 def home():
     return render_template('index.html')
+
+@app.route('/student/dash')
+def student_dash():
+    return render_template('student_dash.html')
+
+@app.route('/professor/dash')
+def professor_dash():
+    return render_template('professor_dash.html')
+
+@app.route('/student/class')
+def student_class():
+    return render_template('student_class.html')
+
+@app.route('/professor/class')
+def professor_class():
+    return render_template('professor_class.html')
+
+@app.route('/student/<username>')
+def profile(username):
+    return render_template('username.html', username=username)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
